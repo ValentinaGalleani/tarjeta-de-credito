0c6c041de12be85bd9cb189e121c371fabb9c75a
ValentinaGalleani <v.galleani.vg@gmail.com>
2017-10-05 11:22:15 -0300

    subiendo el codigo tarjeta de credito

---
+++ Producto final/Tarjeta de credito/app.js
@@ -0,0 +1,6 @@
+	function isValidCard(number, string){ //primero se creará una funcion que contiene number y string
+	var numberCreditCard;/*crearemos la variable, llamada numberCreditCard, y luego por medio de un prompt, 
+	se le preguntara al usuario el número de su tarjeta de credito*/
+		numberCreditCard= prompt("Bienvenid@s a nuestra página web, porfavor inserte el número de su tarjeta de crédito","");
+	}
+// ahora por medio de un
